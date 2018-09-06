cc320df63a66286bf4255ed4b3e453d14c5a0ec0
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-09-06 19:27:10 +0530

    Add files via upload

---
+++ ak62.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n,i,j;
+cout <<"enter a number";
+cin >> n;
+for(i=0;i<n;i++){
+
+for(j=n;j>=i+1;j--){
+cout <<"*";
+  }
+cout <<endl;
+ }
+return 0;
+}
+ 
